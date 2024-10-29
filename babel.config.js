543160f50a4badcb1f939ef55923aba96e7e700e
realrezasadeghi <engrezasadeghii@gmail.com>
2024-10-30 02:31:40 +0330

    chore : install pkgs and add path alias and update babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
 };
