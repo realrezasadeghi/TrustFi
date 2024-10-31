4e3b86346daf024031fc23e2a003ccee14014271
realrezasadeghi <engrezasadeghii@gmail.com>
2024-10-31 18:52:52 +0330

    chore : install safe-area-content in project

--- App.tsx
+++ App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
