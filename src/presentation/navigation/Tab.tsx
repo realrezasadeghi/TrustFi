74b3e3675414ec36d6cd306bb1dd7b46b51291d9
realrezasadeghi <engrezasadeghii@gmail.com>
2024-11-03 21:53:39 +0330

    refactor(Tab.tsx): remove component customTabBar and use default TabBarIcon

--- src/presentation/navigation/Tab.tsx
+++ src/presentation/navigation/Tab.tsx
@@ -1,3 +1,4 @@
+import { StyleSheet } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
@@ -7,7 +8,6 @@ import {
   ProfileScreen,
   RewardsScreen,
 } from '@/presentation/screens';
-import { StyleSheet, Text, View } from 'react-native';
 
 type TabParamList = {
   Home: undefined;
@@ -19,42 +19,32 @@ type TabParamList = {
 
 type IconLabelMapping = Record<keyof TabParamList, { icon: string; label: string }>;
 
-type CustomTabBarIconProps = {
-  icon: string;
-  color: string;
-  label: string;
-  size: number;
-};
-
 const ICON_LABEL_MAPPING: IconLabelMapping = {
-  Home: { icon: 'home', label: 'Home' },
-  Rewards: { icon: 'gift', label: 'Rewards' },
-  Profile: { icon: 'person', label: 'Profile' },
+  Home: { icon: 'home-outline', label: 'Home' },
+  Rewards: { icon: 'gift-outline', label: 'Rewards' },
+  Profile: { icon: 'person-outline', label: 'Profile' },
   Market: { icon: 'trending-up', label: 'Market' },
-  Portfolio: { icon: 'briefcase', label: 'Portfolio' },
+  Portfolio: { icon: 'briefcase-outline', label: 'Portfolio' },
 };
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
-const CustomTabBarIcon: React.FC<CustomTabBarIconProps> = ({ icon, color, label, size }) => (
-  <View style={styles.iconContainer}>
-    <Ionicons name={icon} size={size} color={color} />
-    <Text style={[styles.iconLabel, { color }]}>{label}</Text>
-  </View>
-);
-
 export default function TabNavigation() {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarStyle: styles.tabBarStyle,
-        tabBarIcon: ({ color, size }) => {
-          const { icon, label } = ICON_LABEL_MAPPING[route.name];
-          return <CustomTabBarIcon size={size} icon={icon} color={color} label={label} />;
-        },
-      })}>
+      screenOptions={({ route }) => {
+        const { icon, label } = ICON_LABEL_MAPPING[route.name]
+        return {
+          tabBarLabel: label,
+          headerShown: false,
+          tabBarHideOnKeyboard: true,
+          tabBarStyle: styles.tabBarStyle,
+          tabBarLabelStyle: styles.tabBarLabel,
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name={icon} size={size} color={color} />
+          },
+        }
+      }}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Portfolio" component={PortfolioScreen} />
       <Tab.Screen name="Rewards" component={RewardsScreen} />
@@ -65,19 +55,14 @@ export default function TabNavigation() {
 }
 
 const styles = StyleSheet.create({
+  tabBarLabel: {
+    fontSize: 12,
+  },
   tabBarStyle: {
     height: 70,
     elevation: 0,
     paddingTop: 10,
     borderTopWidth: 0,
     paddingBottom: 10,
-  },
-  iconContainer: {
-    gap: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  iconLabel: {
-    fontSize: 12,
   },
 });
