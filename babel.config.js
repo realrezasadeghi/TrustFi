338eeac82b84a9f0a40c6d53f2015eb7e458d67b
realrezasadeghi <engrezasadeghii@gmail.com>
2024-11-07 13:45:25 +0330

    chore: install react-native-reanimated in project

--- babel.config.js
+++ babel.config.js
@@ -17,5 +17,6 @@ module.exports = {
         path: '.env',
       },
     ],
+    'react-native-reanimated/plugin'
   ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
 
 /**
  * Metro configuration
@@ -8,4 +10,4 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = wrapWithReanimatedMetroConfig(mergeConfig(getDefaultConfig(__dirname), config));
